0662af75328765d6a798a634476f3f8ee8078061
valsimplon <val.simplon@gmail.com>
2017-05-03 09:51:20 +0200

    ok methodes get et post

---
+++ src/main/java/co/simplon/poleEmploi/decouverteServlets/Hello.java
@@ -0,0 +1,46 @@
+package co.simplon.poleEmploi.decouverteServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+	
+	private String message;
+	private String nom;
+
+	public void init() throws ServletException {
+		// Do required initialization
+		message = "Hello ";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+
+		nom = request.getParameter("NOM");
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message +  nom + "</h1>");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+
+		nom = request.getParameter("NOM");
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message +  nom + "</h1>");
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+}
--- src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
+++ src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld extends HttpServlet {
 
 	public void init() throws ServletException {
 		// Do required initialization
-		message = "Hello World";
+		message = "Hello World everybody";
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
