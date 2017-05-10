78c3ac678dcc84602c5bc37aff9aa317fe63e8f4
valsimplon <val.simplon@gmail.com>
2017-05-10 10:51:37 +0200

    travail sur jsp

--- src/main/java/co/simplon/poleEmploi/decouverteServlets/Hello.java
+++ src/main/java/co/simplon/poleEmploi/decouverteServlets/Hello.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Hello extends HttpServlet {
-	
+
 	private String message;
 
 	public void init() throws ServletException {
@@ -20,44 +20,47 @@ public class Hello extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setContentType("text/html");
 
 		String nom_ = request.getParameter("NOM");
-		
+
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message +  nom_ + "</h1>");
+		out.println("<h1>" + message + nom_ + "</h1>");
 	}
-	
-	 public  void doPost(HttpServletRequest request, HttpServletResponse response)
-		     throws ServletException, IOException {
-		    
-		     // construction d'un request dispatcher sur le chemin hello.jsp, qui doit exister
-		     // dans la web application courante
-		    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/hello.jsp") ;
-		    
-		     // redirection de la requête vers cette ressource
-		    String nom = "world";
-		    if (request.getParameter("NOM") != null && !request.getParameter("NOM").isEmpty()) {
-		    	nom = request.getParameter("NOM");
-		    }
-	    	request.setAttribute("leNom", nom);
-		    requestDispatcher.forward(request, response) ;
-		    
-		     // cette servlet perd la main pour le traitement de la requête courante
-		}
-	
-	/*	public void doPost(HttpServletRequest request, HttpServletResponse response)
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
+		// construction d'un request dispatcher sur le chemin hello.jsp, qui
+		// doit exister
+		// dans la web application courante
+		RequestDispatcher requestDispatcher = request
+				.getRequestDispatcher("/WEB-INF/hello.jsp");
 
-		nom = request.getParameter("NOM");
-		
-		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message +  nom + "</h1>");
+		// redirection de la requête vers cette ressource
+		String nom = "world";
+		if (request.getParameter("NOM") != null
+				&& !request.getParameter("NOM").isEmpty()) {
+			nom = request.getParameter("NOM");
+		}
+		request.setAttribute("leNom", nom.toUpperCase());
+		requestDispatcher.forward(request, response);
+
+		// cette servlet perd la main pour le traitement de la requête courante
 	}
-	*/
+
+	/*
+	 * public void doPost(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * 
+	 * response.setContentType("text/html");
+	 * 
+	 * nom = request.getParameter("NOM");
+	 * 
+	 * PrintWriter out = response.getWriter(); out.println("<h1>" + message +
+	 * nom + "</h1>"); }
+	 */
 
 	public void destroy() {
 		// do nothing.
--- src/main/webapp/WEB-INF/hello.jsp
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -7,9 +7,36 @@
 <title>HELLO JSP</title>
 </head>
 <body>
-	<h1>Bonjour à toi, ${ param["NOM"] } !</h1>
-	<h1>Bonjour bonjour ${ leNom }</h1>
-	<h2>${ header["user-agent"] }</h2>
+	<h1>1 - Bonjour à toi, ${ param["NOM"] } !</h1>
+	<h1>2 - Bonjour bonjour ${ leNom }</h1>
+	<h1>3 - Bonjour bonjour ${ param.NOM }</h1>
+	<h1>4 - Bonjour bonjour ${ requestScope.leNom }</h1>
+	<h1>5 -	<%=request.getParameter("NOM")%></h1>
+	<h1>6 -	<%=request.getAttribute("leNom")%></h1>
+
+	<h2>header 1 - ${ header["user-agent"] }</h2>
+	<h2>header 2 - <%=request.getHeader("user-agent")%></h2>
+
+	<h2>ContextPath - <%=request.getContextPath()%></h2>
+
+	<h3>cookie - ${ cookie }</h3>
+
+	<div id="demo">
+		<script>
+			var txt = "Quelques informations sur le navigateur en javaScript";
+
+			txt += "<p>User-agent header: " + navigator.userAgent + "</p>";
+			txt += "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+			txt += "<p>Browser Name: " + navigator.appName + "</p>";
+			txt += "<p>Browser Version: " + navigator.appVersion + "</p>";
+			txt += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+			txt += "<p>Browser Language: " + navigator.language + "</p>";
+			txt += "<p>Browser Online: " + navigator.onLine + "</p>";
+			txt += "<p>Platform: " + navigator.platform + "</p>";
+
+			document.getElementById("demo").innerHTML = txt;
+		</script>
+	</div>
 
 </body>
 </html>
